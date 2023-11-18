ae883dd72a3b42bf4f81d3cdd16bb7a92c2fbe23
PawanSirsat <p1.sirsat1998@gmail.com>
2023-11-18 10:00:45 +0530

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Social-Media-React-Appwrite",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
