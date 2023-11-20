b42c71a91dc06c1168a51fdf252b6b6c8571816c
PawanSirsat <p1.sirsat1998@gmail.com>
2023-11-20 12:32:16 +0530

    Update Image

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -63,7 +63,7 @@ const SigninForm = () => {
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo6.png" width="350" alt="logo" />
 
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-2">
           Log in to your account
         </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -87,9 +87,9 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo5.png" width="350" alt="logo" />
+        <img src="/assets/images/logo6.png" width="350" alt="logo" />
 
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-2">
           Create a new account
         </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
