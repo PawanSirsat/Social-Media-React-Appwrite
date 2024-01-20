522266928cc94d9cded11e7efa938c055b33f95f
PawanSirsat <p1.sirsat1998@gmail.com>
2024-01-20 22:12:42 +0530

    Commit

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -140,7 +140,6 @@ const PostDetails = () => {
 
       <div className="w-full max-w-5xl">
         <hr className="border w-full border-dark-4/80" />
-
         <h3 className="body-bold md:h3-bold w-full my-10">
           More Related Posts
         </h3>
